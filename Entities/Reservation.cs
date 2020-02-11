ffca31ed3c83a3312cdc3c94df6a9a0f46a56b95
“valeskaandrade” <valeska.andrade@gmail.com>
2020-02-11 15:39:43 -0300

    aula 142 programa de reserva de quarto sem trat excecao

---
+++ Entities/Reservation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace aula142TratExcecao.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime Checkin { get; set; }
+        public DateTime Checkout { get; set; }
+
+        public int Duration()
+        {
+            TimeSpan duration = Checkout.Subtract(Checkin);
+            return (int) duration.TotalDays;
+        }
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout)
+        {
+            RoomNumber = roomNumber;
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        public void UpdateDates( DateTime newCheckin, DateTime newCheckout)
+        {
+            Checkin = newCheckin;
+            Checkout = newCheckout;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+          
+            sb.Append("Reservation: Room: ");
+            sb.Append(RoomNumber.ToString());
+            sb.Append(", check-in: ");
+            sb.Append(Checkin.ToString());
+            sb.Append(",check-out: ");
+            sb.Append(Checkout.ToString());
+            sb.Append(", ");
+            sb.Append(Duration());
+            sb.Append(" nights");
+            return sb.ToString();
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using aula142TratExcecao.Entities;
+
+namespace aula142TratExcecao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programa de reserva de quarto sem tratamento de exceção 
+          
+                Console.WriteLine("Informe os dados da reserva");
+
+                Console.Write("Número do Quarto: ");
+                int roomNumber = int.Parse(Console.ReadLine());
+
+                Console.Write("Data de entrada (dd/MM/yyyy): ");
+                DateTime checkin = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Data de saída (dd/MM/yyyy): ");
+                DateTime checkout = DateTime.Parse(Console.ReadLine());
+                if (checkout <= checkin)
+                {
+                    Console.WriteLine("A data da saída deve ser posterior à entrada");
+                }
+                else
+                {               
+                    Reservation reserva;
+                    reserva = new Reservation(roomNumber, checkin, checkout);
+
+                    Console.WriteLine(reserva.ToString());
+                
+
+                Console.WriteLine("Informe os dados da reserva para atualização");
+                Console.Write("Nova Data de entrada (dd/MM/yyyy): ");
+
+                DateTime novoCheckin = DateTime.Parse(Console.ReadLine());
+                Console.Write("Nova Data de saída (dd/MM/yyyy): ");
+                DateTime novoCheckout = DateTime.Parse(Console.ReadLine());
+
+                if (novoCheckin <= DateTime.Today)
+                {
+                    Console.WriteLine("A nova data de chegada deve ser posterior à hoje");
+                }
+                if (novoCheckout <= DateTime.Today)
+                {
+                    Console.WriteLine("A nova data de saída deve ser posterior à hoje");
+                }
+
+                reserva.UpdateDates(novoCheckin, novoCheckout);
+                Console.WriteLine(reserva.ToString());
+                }
+         
+
+        }
+    }
+}
